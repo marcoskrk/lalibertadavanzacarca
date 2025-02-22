6cbd631e6d60c8768881ebbfa9d9dc2c4c658900
marcoskrk <81986191+marcoskrk@users.noreply.github.com>
2025-02-22 18:13:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// Obtén el botón del menú y el menú desplegable
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
-
-// Añadir el evento de clic para alternar el menú
-menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('active'); // Esto agregará o quitará la clase 'active' que controlará la visibilidad del menú
+// script.js
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    document.getElementById("menu").classList.toggle("active");
 });
