6c270ea853fe241006d47ec707b31d2b80c60091
marcoskrk <81986191+marcoskrk@users.noreply.github.com>
2025-02-22 20:26:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Obtén el botón del menú y el menú desplegable
+// JavaScript para alternar el menú al hacer clic en el botón hamburguesa
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
-
-// Añadir el evento de clic para alternar el menú
 menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('active'); // Esto agregará o quitará la clase 'active' que controlará la visibilidad del menú
+    menu.classList.toggle('active');
 });
