80b1b47e357445e93eb7656ff7e8dd2e53fcba65
marcoskrk <81986191+marcoskrk@users.noreply.github.com>
2025-02-22 18:38:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Código JavaScript para mostrar/ocultar el menú al hacer clic en el botón hamburguesa
-document.getElementById('menu-toggle').addEventListener('click', function() {
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    document.getElementById("menu").classList.toggle("active");
+});
+document.getElementById('menu-toggle').addEventListener("click", function() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('show');
 });
