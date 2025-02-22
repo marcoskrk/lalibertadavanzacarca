b9d701406680eb85759e8ba5021a02e5e7b8df1e
marcoskrk <81986191+marcoskrk@users.noreply.github.com>
2025-02-22 19:40:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Obtener los elementos necesarios
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
 
-// Agregar el evento para mostrar u ocultar el menú cuando se haga clic en el botón hamburguesa
+// Añadir el evento de clic para alternar el menú
 menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('active');
+    menu.classList.toggle('active'); // Esto agregará o quitará la clase 'active' que controlará la visibilidad del menú
 });
